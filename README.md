bd0d90fd8208d1771bab396e2eae67da5fae649b
GranSpencer09 <Granspencer09@gmail.com>
2022-10-02 20:10:56 -0700

    building out client side, specifically the service worker and webpack pages. created gitignore and added to package.json file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Text Editor Starter Code
+
+Uses IndexedDB to create an object store and includes both GET and PUT methods
+
+The application works without an internet connection
+
+Automatically saves content inside the text editor when the DOM window is unfocused
+
+Bundled with webpack
+
+Create a service worker with workbox that Caches static assets
+
+The application should use babel in order to use async / await
+
+Application must have a generated manifest.json using the WebpackPwaManifest plug-in
+
+Can be installed as a Progressive Web Application
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,62 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
+    // Add database schema if it has not already been initialized.
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log("PUT to jate database");
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({
+    id: 1,
+    value: content,
+  });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
+
+// Logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("GET from JATE database");
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction("jate", "readonly");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
